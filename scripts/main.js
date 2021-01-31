f0c7f537c1441d89449630af900ce52c68a88198
SMOLKEYS.exe <75618732+SMOLKEYS@users.noreply.github.com>
2021-01-31 22:02:20 +0800

    Create main.js

---
+++ scripts/main.js
@@ -0,0 +1,5 @@
+UnitTypes.mono.payloadCapacity = 81828817
+UnitTypes.poly.payloadCapacity = 81828817
+UnitTypes.mega.payloadCapacity = 81828817
+UnitTypes.quad.payloadCapacity = 81828817
+UnitTypes.oct.payloadCapacity = 81828817
