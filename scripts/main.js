1f986404d198c678fbc4ec7e348c1b82e6270725
SMOLKEYS.exe <75618732+SMOLKEYS@users.noreply.github.com>
2021-02-04 03:40:17 +0800

    screw numbers, infinity it is

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,3 @@
-UnitTypes.mono.payloadCapacity = Number.MAX_VALUE
-UnitTypes.poly.payloadCapacity = Number.MAX_VALUE
 UnitTypes.mega.payloadCapacity = Number.MAX_VALUE
 UnitTypes.quad.payloadCapacity = Number.MAX_VALUE
 UnitTypes.oct.payloadCapacity = Number.MAX_VALUE
